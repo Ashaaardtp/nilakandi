8d4d6e0c2c533a6bfb58a090fd63b2d9006b3348
Pasha Raditya Putra <ungu55.pr@gmail.com>
2026-04-03 22:54:53 +0700

    Analytic

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }) {
   return (
+    <Analytics />
     <html lang="en">
       <head>
         <link
